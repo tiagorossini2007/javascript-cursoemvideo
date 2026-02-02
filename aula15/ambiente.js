0ee631f508bf2f5041158aadd42e622f0fd97262
tigasrossini <tiagorossinicorrea@gmail.com>
2026-02-02 12:39:13 -0300

    aula 02/02

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,6 +1,12 @@
-const num = []
+let num = [5, 8, 2, 9, 3]
+num.push(1)
 num.sort()
 console.log(num)
-console.log(`O valor tem ${num.length} posições`)
-console.log(`O primeiro valor é ${num[0]}`)
-
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+let pos = num.indexOf(8)
+if (pos == -1) {
+    console.log('O valor não foi encontrado!')
+} else {
+    console.log(`O valor está na posição ${pos}`)
+}
---
+++ aula16/funcao01.js
@@ -0,0 +1,9 @@
+function parImpar(n){
+    if(n % 2 ==0){
+        return 'par'
+    }
+    else{
+        return 'ímpar'
+    }
+}
+console.log(parImpar(7))
